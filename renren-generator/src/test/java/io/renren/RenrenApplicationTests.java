966b1f1a82c31070b4be769681dd3eb8f51410cd
ying <389828503@qq.com>
2017-09-11 16:31:06 +0800

    ffffffffffffffff

--- renren-generator/src/test/java/io/renren/RenrenApplicationTests.java
+++ renren-generator/src/test/java/io/renren/RenrenApplicationTests.java
@@ -6,7 +6,7 @@ import org.junit.runner.RunWith;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-//@SpringBootTest111111111111cccccccccccccccccccccccccccccc33333333333
+//@SpringBootTest111111111111cccccccccccccccccccccccccccccc33333333333111111111111
 public class RenrenApplicationTests {
 
 	@Test
